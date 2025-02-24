2636fe09239236d1085b0bc7fb1a808f101c010f
98donggyu <98donggyu@gmail.com>
2025-02-24 17:00:12 +0900

    부트스트랩 적용과 예외저리

--- main.js
+++ main.js
@@ -6,11 +6,33 @@ function saveHandler()
     let text = $("#text").val();
     let mood = $("#mood").val();
 
-    diary = `<p>${date}: ${text} - ${mood}</p>` + diary;
+    if (date == "")
+    {
+        alert("date를 선택하세요.")
+        return false
+    }
+    if (text == "")
+    {
+        alert("긍정확언을 입력하세요.")
+        return false
+    }
+
+    diary = `<a href="#" class="list-group-item list-group-item-action" aria-current="true">
+    <div class="d-flex w-100 justify-content-between">
+        <h1 class="m-1" style="color: orange;">${text}</h1>
+        <small style="color: purple;">${date}</small>
+    </div>
+    <small>${mood}</small>
+</a>` + diary;
 
     $("#result").html(diary);
 
     localStorage.setItem("diary", diary)
+    let storage1 = localStorage.getItem("diary")
+    console.log(storage1)
+    
+    $("#date").val("");
+    $("#text").val("");
 }
 
 function resetHandler()
@@ -24,10 +46,11 @@ $(document).ready(function()
 {
     diary = localStorage.getItem("diary");
 
-    if (diary == null) {
+    if (diary == null)
+    {
         diary = "";
     }
-
+    
     $("#result").html(diary)
     $("#save").click(saveHandler);
     $("#reset").click(resetHandler);
